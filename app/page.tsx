7f8e5d483116857b75c58aedf6a9db195b0f20a4
Captain Rengoku <souravdash4708@gmail.com>
2025-10-19 23:37:15 +0530

    changed few style for better UI

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         <TodoStats />
         <div className="space-y-2">
           {todos.length === 0 ? (
-            <p className="text-center text-gray-500">No todos</p>
+            <p className="text-center text-gray-500">No todos yet!</p>
           ) : (
             todos.map((todo) => <TodoItem key={todo.id} todo={todo} />)
           )}
--- components/todo-stats.tsx
+++ components/todo-stats.tsx
@@ -13,23 +13,38 @@ const TodoStats = () => {
     setMounted(true);
   }, []);
   if (!mounted) {
-    return <div className="mb-4 text-gray-500">Loading...</div>
+    return (
+      <div className="flex justify-center items-center mt-20 mb-4">
+        <div className="relative flex items-center justify-center w-20 h-20">
+          {/* Rotating ring */}
+          <div className="absolute w-full h-full rounded-lg border-2 border-slate-600 animate-spin"/>
+          <div className="absolute w-full h-full rounded-lg border-2 border-slate-600 rotate-60 animate-spin"/>
+          <div className="absolute w-full h-full rounded-lg border-2 border-slate-600 rotate-120 animate-spin"/>
+          <div className="absolute w-full h-full rounded-lg border-2 border-slate-600 rotate-180 animate-spin"/>
+          {/* Static text */}
+          <span className="text-slate-400">Loading</span>
+          <span className="text-slate-200 animate-pulse">...</span>
+        </div>
+      </div>
+    );
   }
 
   return (
     <div className="mb-4 text-center">
-      <div className="flex w-full justify-end items-center gap-8 px-4 py-2 
-      bg-slate-400/60 rounded-lg select-none">
+      <div
+        className="flex w-full justify-end items-center gap-8 px-4 py-2 
+      bg-slate-400/60 rounded-lg select-none"
+      >
         <div className="flex justify-center items-center gap-1">
-          <ListIcon size={16}/>
+          <ListIcon size={16} />
           <span>Total: {stats.total}</span>
         </div>
         <div className="flex justify-center items-center gap-1">
-          <Hourglass size={16}/>
+          <Hourglass size={16} />
           <span>Active: {stats.active}</span>
         </div>
         <div className="flex justify-center items-center gap-1">
-          <ListChecks size={16}/>
+          <ListChecks size={16} />
           <span>Completed: {stats.completed}</span>
         </div>
       </div>
